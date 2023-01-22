0cbc6fbe714929abe38cc04b26a3942b7d806fd0
jasabira <jasbircoro.dev@gmail.com>
2023-01-22 10:16:19 -0300

    agregue postmapping del registro

--- src/main/java/com/practicas/personal/controllers/PersonalController.java
+++ src/main/java/com/practicas/personal/controllers/PersonalController.java
@@ -6,6 +6,8 @@ import com.practicas.personal.services.PersonalService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class  PersonalController{
@@ -30,6 +32,12 @@ public class  PersonalController{
         Personal personal = new Personal();
         model.addAttribute("personal", personal);
         return "registro";
+    }
+
+    @PostMapping("/registro")
+    public String crearPersonal(@ModelAttribute("personal") Personal personal){
+        personalService.guardarPersonal(personal);
+        return "redirect:/login";
     }
 
     @GetMapping("/login/lista_admin")
