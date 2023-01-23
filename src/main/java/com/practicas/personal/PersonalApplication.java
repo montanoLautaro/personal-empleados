fa686e8bf748656f28c3834e000830ccaefc5ed6
Montaño Heredia Lautaro Nahuel <88169590+nahue62@users.noreply.github.com>
2023-01-23 10:25:24 -0300

    Update PersonalApplication.java

--- src/main/java/com/practicas/personal/PersonalApplication.java
+++ src/main/java/com/practicas/personal/PersonalApplication.java
@@ -12,7 +12,7 @@ public class PersonalApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(PersonalApplication.class, args);
 		PersonalRepository personalRepository = context.getBean(PersonalRepository.class);
-		personalRepository.save(new Personal("El", "Pez", "qwerty", "123", "admin"));
+		//personalRepository.save(new Personal("El", "Pez", "qwerty", "123", "admin"));
 	}
 
 }
